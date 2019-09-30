09ff299f107af8a08a33738e697dc108d22da792
sharondsza26 <sharondsouza26@gmail.com>
2019-09-30 18:59:55 +0530

    My First

---
+++ Myfirstprogram.java
@@ -0,0 +1,11 @@
+class Myfirstprogram
+{
+ public static void main(String[] args)
+ {
+  System.out.println("St.Francis Institute of Technology");
+  
+  System.out.println("SV road, Borivli");
+  System.out.println("Mumbai-103");
+ 
+ }
+} 
