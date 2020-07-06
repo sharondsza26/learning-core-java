f61c89912df165d4f89d3d669ac33fae57f55ab5
sharondsza26 <sharondsouza26@gmail.com>
2020-07-06 19:42:37 +0530

    Adding a panel with button

---
+++ ComponentsInGui/Panel1.java
@@ -0,0 +1,24 @@
+package ComponentsInGui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel1 {
+    
+    public static void main(String[] args) {
+        Panel1 gui = new Panel1();
+        gui.go();
+    }
+
+    public void go(){
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        JButton button = new JButton("Shock me");
+
+        panel.add(button);
+        panel.setBackground(Color.darkGray);
+        frame.getContentPane().add(BorderLayout.EAST, panel);
+        frame.setSize(250,200);
+        frame.setVisible(true);
+    }
+}
