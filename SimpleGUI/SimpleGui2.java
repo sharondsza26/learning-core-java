1bcd74d9ad76e25ca473be9d12c7b7467d002f8f
sharondsza26 <sharondsouza26@gmail.com>
2020-07-06 19:37:29 +0530

    Checking if button is clicked

---
+++ SimpleGUI/SimpleGui2.java
@@ -0,0 +1,32 @@
+package SimpleGUI;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class SimpleGui2 implements ActionListener {
+    JButton button;
+
+    public static void main(String[] args ) {
+        SimpleGui2 gui = new SimpleGui2();
+        gui.go();
+    }
+
+    public void go(){
+        JFrame frame = new JFrame();
+        button = new JButton("click me");
+
+        button.addActionListener(this);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(button);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+        
+    }
+
+    public void actionPerformed(ActionEvent event){
+        button.setText("Ive been clicked!");
+    }
+
+}
+
